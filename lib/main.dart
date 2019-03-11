28df3ebd96d2c62e3e1c3ec0cdb8cee75d82c524
maoai_xianyu <194264514@qq.com>
2019-03-11 23:38:32 +0800

    sliver

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'demo/listview_demo.dart';
 import 'demo/basic_demo.dart';
 import 'demo/layout_demo.dart';
 import 'demo/view_demo.dart';
-
+import 'demo/sliver_demo.dart';
 /* void main(){
   runApp(App());
 } */
@@ -87,7 +87,7 @@ class Home extends StatelessWidget {
             //Icon(Icons.change_history, size: 128.0, color: Colors.black12),
             LayoutDemo(),
             //Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
-            ViewDemo(),
+            SliverDemo(),
             //Icon(Icons.view_quilt, size: 128.0, color: Colors.black12),
           ],
         ),
