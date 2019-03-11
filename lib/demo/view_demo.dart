bf6a2b70b3c77df76c1955114eec9f5c7fba2adb
maoai_xianyu <194264514@qq.com>
2019-03-11 22:23:55 +0800

    页面视图和网格视图

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -1,10 +1,147 @@
 import 'package:flutter/material.dart';
+import '../model/post.dart';
 
 class ViewDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return PageViewDemo();
+    return GridViewBuilderDemo();
+  }
+}
+
+/**
+ * 网格视图 GridView.count 默认可以垂直滚动
+ */
+class GridViewBuilderDemo extends StatelessWidget {
+  Widget _gridItemBuilder(BuildContext context, int index) {
+    return Container(
+      child: Image.network(
+        posts[index].imageUrl,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return GridView.builder(
+      padding: EdgeInsets.all(8.0),
+      itemCount: posts.length,
+      itemBuilder: _gridItemBuilder,
+      // 相当于 GridView.count
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 8.0,
+        mainAxisSpacing: 8.0,
+      ),
+    );
+  }
+}
+
+/**
+ * 网格视图 GridView.count 默认可以垂直滚动
+ */
+class GridViewExtentDemo extends StatelessWidget {
+  List<Widget> _buildTiles(int length) {
+    return List.generate(length, (int index) {
+      return Container(
+        color: Colors.grey[300],
+        alignment: Alignment(0.0, 0.0),
+        child: Text(
+          'Item $index',
+          style: TextStyle(fontSize: 18.0, color: Colors.grey),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return GridView.extent(
+      maxCrossAxisExtent: 150.0,
+      crossAxisSpacing: 16.0,
+      mainAxisSpacing: 16.0,
+      // 水平方向
+      // scrollDirection: Axis.horizontal,
+      children: _buildTiles(100),
+    );
+  }
+}
+
+/**
+ * 网格视图 GridView.count 默认可以垂直滚动
+ */
+class GridViewCountDemo extends StatelessWidget {
+  List<Widget> _buildTiles(int length) {
+    return List.generate(length, (int index) {
+      return Container(
+        color: Colors.grey[300],
+        alignment: Alignment(0.0, 0.0),
+        child: Text(
+          'Item $index',
+          style: TextStyle(fontSize: 18.0, color: Colors.grey),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return GridView.count(
+      // 交叉轴上显示的项目的数量
+      crossAxisCount: 3,
+      crossAxisSpacing: 16.0,
+      mainAxisSpacing: 16.0,
+      // 水平方向
+      scrollDirection: Axis.horizontal,
+      children: _buildTiles(100),
+    );
+  }
+}
+
+/**
+ * pageview.Builder
+ */
+class PageViewBuilderDemo extends StatelessWidget {
+  Widget _pageItemBuilder(BuildContext content, int index) {
+    return Stack(
+      children: <Widget>[
+        SizedBox.expand(
+          child: Image.network(
+            posts[index].imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Positioned(
+          bottom: 8.0,
+          left: 8.0,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                posts[index].title,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                posts[index].author,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return PageView.builder(
+      itemCount: posts.length,
+      itemBuilder: _pageItemBuilder, // 值是一个方法
+    );
   }
 }
 
@@ -12,7 +149,7 @@ class ViewDemo extends StatelessWidget {
  * 页面视图 PageView  
  * 1. 页面视图的属性
  * 2. 使用PageController调整页面
- * */ 
+ * */
 class PageViewDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -23,7 +160,7 @@ class PageViewDemo extends StatelessWidget {
       // 倒置
       //reverse: true,
       // 水平或者垂直滚动
-      //scrollDirection: Axis.vertical,
+      // scrollDirection: Axis.vertical,
       // 页面在可视口的中间显示后会触发回调
       onPageChanged: (currentPage) => debugPrint('Page: $currentPage'),
 
@@ -33,7 +170,6 @@ class PageViewDemo extends StatelessWidget {
         keepPage: false,
         // 页面占用可视口的比例
         viewportFraction: 0.85,
-
       ),
 
       children: <Widget>[
@@ -53,7 +189,7 @@ class PageViewDemo extends StatelessWidget {
             style: TextStyle(fontSize: 32.0, color: Colors.white),
           ),
         ),
-         Container(
+        Container(
           color: Colors.blueGrey[900],
           alignment: Alignment(0.0, 0.0),
           child: Text(
