0b76243bdd78a3708279ea5a0fb9c8c4828dec1a
zhangkun@boxfish.cn <194264514@qq.com>
2019-04-15 16:48:23 +0800

    开发准备

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'model/post.dart';
-
 /* void main(){
   runApp(App());
 } */
@@ -10,6 +8,7 @@ void main() => runApp(App());
 // 自定义的小部件是一个类
 // 1. 继承 sfw StatefulWidget 需要修改数据的的小部件
 // 2. 继承 ssw StatelessWidget 不需要修改数据的小部件
+
 
 class App extends StatelessWidget {
   @override
@@ -24,22 +23,6 @@ class App extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
-  Widget _listItemBuilder(BuildContext context, int index) {
-    return Container(
-      color: Colors.white,
-      margin: EdgeInsets.all(8.0), //外边距
-      child: Column(
-        children: <Widget>[
-          Image.network(posts[index].imageUrl), //加载图片
-          SizedBox(height: 16.0), //空间高度
-          Text(posts[index].title, style: Theme.of(context).textTheme.title),
-          Text(posts[index].author, style: Theme.of(context).textTheme.subhead),
-          SizedBox(height: 16.0)
-        ],
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -50,10 +33,7 @@ class Home extends StatelessWidget {
         title: Text('coding type'), // 标题
         elevation: 0.0, // 阴影 0.0  默认是 4.0
       ),
-      body: ListView.builder(
-        itemCount: posts.length,
-        itemBuilder: _listItemBuilder,
-      ),
+      body: Hello(),
     );
   }
 }
