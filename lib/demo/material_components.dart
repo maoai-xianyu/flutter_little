d0986b692af61de242fc244f7ac76131763b4b4f
maoaixianyu <194264514@qq.com>
2019-04-22 23:31:13 +0800

    flatButton  RaisedButton

--- lib/demo/material_components.dart
+++ lib/demo/material_components.dart
@@ -10,21 +10,162 @@ class MaterialComponents extends StatelessWidget {
       ),
       body: ListView(
         children: <Widget>[
-
+          ListItem(
+            title: 'Button',
+            page: ButtonDemo(),
+          ),
+          ListItem(
+            title: 'FloatingActionButton',
+            page: FloatingActionButtonDemo(),
+          ),
         ],
       ),
     );
   }
 }
 
-class ListItem extends StatelessWidget {
+class ButtonDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Widget flatButtonDemo = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        // 文字按钮
+        FlatButton(
+          child: Text('Button'),
+          onPressed: () {},
+          splashColor: Colors.grey, // 渐墨效果
+          textColor: Theme.of(context).accentColor,
+        ),
+        FlatButton.icon(
+          icon: Icon(Icons.add),
+          label: Text('Button'),
+          onPressed: () {},
+          splashColor: Colors.grey, // 渐墨效果
+          textColor: Theme.of(context).accentColor,
+        ),
+      ],
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ButtonDemo'),
+        elevation: 0.0,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                // 按钮有颜色和阴影
+                RaisedButton(
+                  child: Text('Button'),
+                  onPressed: () {},
+                  splashColor: Colors.grey, // 渐墨效果
+                  elevation: 0.0,
+                  textColor: Theme.of(context).accentColor,
+                ),
+                SizedBox(
+                  width: 20.0,
+                ),
+                RaisedButton.icon(
+                  icon: Icon(Icons.add),
+                  label: Text('Button'),
+                  onPressed: () {},
+                  elevation: 12.0,
+                  splashColor: Colors.grey, // 渐墨效果
+                  textColor: Theme.of(context).accentColor,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _WidgetDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ListItem'),
+        title: Text('_WidgetDemo'),
         elevation: 0.0,
       ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FloatingActionButtonDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Widget _floatingActionButton = FloatingActionButton(
+      onPressed: () {},
+      child: Icon(Icons.add),
+      elevation: 0.0, //阴影效果不见，和下一层贴合
+      backgroundColor: Colors.black87, //按钮的背景颜色
+      // 改变形状
+      /* shape: BeveledRectangleBorder(
+        borderRadius: BorderRadius.circular(30.0),
+      ), */
+    );
+
+    // Extende 漂浮动作的按钮
+    final Widget _floatingActionButtonExtended = FloatingActionButton.extended(
+      onPressed: () {},
+      icon: Icon(Icons.add),
+      label: Text('Add'),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FloatingActionButtonDemo'),
+        elevation: 0.0,
+      ),
+      floatingActionButton: _floatingActionButton,
+      floatingActionButtonLocation: FloatingActionButtonLocation
+          .centerDocked, // 和bottomNavigationBar契合，缺口
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 80.0,
+        ),
+        // 和 floatingActionButtonLocation 的缺口
+        shape: CircularNotchedRectangle(),
+      ),
+    );
+  }
+}
+
+class ListItem extends StatelessWidget {
+  final String title;
+  final Widget page;
+
+  ListItem({
+    this.title,
+    this.page,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(title),
+      onTap: () {
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => page));
+      },
     );
   }
 }
