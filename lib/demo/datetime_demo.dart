03e85f88a4d26da5c301afb0cd0845aab14e2d5e
zhangkun@boxfish.cn <194264514@qq.com>
2019-07-11 18:11:45 +0800

    future 异步 async 和等待  await

--- lib/demo/datetime_demo.dart
+++ lib/demo/datetime_demo.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'dart:async';
 
 class DateTimeDemo extends StatefulWidget {
   @override
@@ -8,8 +9,10 @@ class DateTimeDemo extends StatefulWidget {
 
 class _DateTimeDemoState extends State<DateTimeDemo> {
   DateTime selectedDate = DateTime.now();
+  TimeOfDay selectedTime = TimeOfDay(hour: 9, minute: 30);
 
-  void _selectDate() async {
+  // 日期选择器
+  Future<void> _selectDate() async {
     final DateTime date = await showDatePicker(
       firstDate: DateTime(1900),
       lastDate: DateTime(2100),
@@ -21,6 +24,22 @@ class _DateTimeDemoState extends State<DateTimeDemo> {
     }
     setState(() {
       selectedDate = date;
+    });
+  }
+
+  // 异步编程  返回的future  可以用 异步标签 async 标注方法为异步方法  await 是等待异步编程的结果
+
+  // 时间选择器
+  Future<void> _selectTime() async {
+    final TimeOfDay time = await showTimePicker(
+      context: context,
+      initialTime: selectedTime,
+    );
+    if (time == null) {
+      return;
+    }
+    setState(() {
+      selectedTime = time;
     });
   }
 
@@ -39,12 +58,27 @@ class _DateTimeDemoState extends State<DateTimeDemo> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text(DateFormat.yMMMd().format(selectedDate)),
                 InkWell(
                   onTap: () {
                     _selectDate();
                   },
-                  child: Icon(Icons.arrow_drop_down),
+                  child: Row(
+                    children: <Widget>[
+                      Text(DateFormat.yMMMd().format(selectedDate)),
+                      Icon(Icons.arrow_drop_down),
+                    ],
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    _selectTime();
+                  },
+                  child: Row(
+                    children: <Widget>[
+                      Text(selectedTime.format(context)),
+                      Icon(Icons.arrow_drop_down),
+                    ],
+                  ),
                 ),
               ],
             ),
